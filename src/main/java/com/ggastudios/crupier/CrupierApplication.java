8cf7f8cb5368b6124da6449271473aa4e7979c5b
Unknown <gmorenooliva@gmail.com>
2020-04-10 20:28:44 +0200

    cambio de version de spring boot admin

--- src/main/java/com/ggastudios/crupier/CrupierApplication.java
+++ src/main/java/com/ggastudios/crupier/CrupierApplication.java
@@ -1,8 +1,8 @@
 package com.ggastudios.crupier;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
